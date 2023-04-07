1e123a9a0ac3ef5a51f185dfd7efde56d22cf66d
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-04-07 23:42:35 +0200

    Adds Terraform provider + resource for lambda

---
+++ infra/main.tf
@@ -0,0 +1,23 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "whenDoTheyPlay.py"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn # What? is this?
+  handler       = "index.test" # What is this..?
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256 # This?
+
+}
