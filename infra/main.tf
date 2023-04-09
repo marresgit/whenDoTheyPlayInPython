f22d915d2fe0655871d4ac4568324140b3515bf1
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-04-09 22:26:03 +0200

    Adds workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   version = "~> 4.0"
-  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
   region  = "us-east-1"
 }
 
