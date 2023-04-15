ccc488acdffb370f011c89684ef428344f2f945c
Mauricio Saez <saeztoro.mauricio@gmail.com>
2023-04-15 22:19:50 +0200

    Adds resource for: aws iam role, iam_policy_document??, archive_file and aws_lambda_function

--- infra/main.tf
+++ infra/main.tf
@@ -19,24 +19,40 @@ provider "aws" {
 }
 
 
+#Lambda creation:
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
 
-# TODO: send state file to bucket from commandline. THEN do this ^
+data "archive_file" "zip_the_python_file" {
+  type        = "zip"
+  source_dir = "${path.module}/app/"
+  output_path = "${path.module}/app/whenDoTheyPlay.zip"
+}
 
-#data "archive_file" "lambda" {
-#  type        = "zip"
-#  source_file = "whenDoTheyPlay.py"
-#  output_path = "lambda_function_payload.zip"
-#}
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "${path.module}/app/lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "whenDoTheyPlay.player" # [name of file].[name of def]
+  runtime       = "python3.8"
 
-#resource "aws_lambda_function" "test_lambda" {
-#  # If the file is not in the current working directory you will need to include a
-#  # path.module in the filename.
-#  filename      = "lambda_function_payload.zip"
-#  function_name = "lambda_function_name"
-#  role          = aws_iam_role.iam_for_lambda.arn # What? is this?
-#  handler       = "index.test" # What is this..?
-#
-#  source_code_hash = data.archive_file.lambda.output_base64sha256 # This?
-#
-#}
+
+}
